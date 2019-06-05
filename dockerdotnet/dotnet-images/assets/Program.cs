ac06da912ed82756fa3127458634acc2ca686a81
Sidney Andrews <sidney@seesharprun.net>
2019-06-05 10:03:52 -0400

    Update Program.cs

--- dockerdotnet/dotnet-images/assets/Program.cs
+++ dockerdotnet/dotnet-images/assets/Program.cs
@@ -1,7 +1,7 @@
-public class Program
+public sealed class Program
 {
     public static async System.Threading.Tasks.Task Main(string[] args)
     {
-        await System.Console.Out.WriteLineAsync("Hello KataCoda!");
+        await System.Console.Out.WriteLineAsync("Hello .NET Core on KataCoda!");
     }
 }
